6cdec6be7c21bc12ce7e7aeb2f81ca3b8d134590
longfn <longpn.golsoftvn@gmail.com>
2022-07-21 07:06:45 +0000

    composer require doctrine/dbal, try various things with migrate

---
+++ database/migrations/2022_07_21_053940_create_users_table.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id('id');
+            $table->string('name');
+            $table->string('email');
+            $table->string('password');
+            $table->string('address');
+            $table->string('phone');
+            $table->string('fblink');
+            $table->string('ytlink');
+            $table->timestamps();
+        });
+        if (Schema::hasColumn('users', 'name')) {
+            Schema::table('users', function (Blueprint $table) {
+                $table->renameColumn('name', 'fullname');
+            });
+        }
+        if (Schema::hasColumn('users', 'ytlink')) {
+            Schema::table('users', function (Blueprint $table) {
+                $table->dropColumn('ytlink');
+            });
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+};
