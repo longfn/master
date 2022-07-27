fd45f2ae001350da14452ffdc60133261bb08360
longfn <longpn.golsoftvn@gmail.com>
2022-07-27 06:19:38 +0000

    Requires email verification to access certain routes

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,16 +3,18 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
-use Illuminate\Contracts\Auth\Authenticatable;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Auth\Authenticatable as AuthenticableTrait;
+use Illuminate\Notifications\Notifiable;
 
-class User extends Model implements Authenticatable
+class User extends Authenticatable implements MustVerifyEmail
 {
     use HasFactory;
     use SoftDeletes;
     use AuthenticableTrait;
+    use Notifiable;
 
     protected $fillable = [
         'name',
@@ -52,5 +54,17 @@ class User extends Model implements Authenticatable
     public function messages()
     {
         return $this->hasMany(Message::class);
+    }
+
+    public function hasVerifiedEmail()
+    {
+        return ! is_null($this->verified_at);
+    }
+
+    public function markEmailAsVerified()
+    {
+        return $this->forceFill([
+            'verified_at' => $this->freshTimestamp(),
+        ])->save();
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Admin\ProductController;
 use App\Http\Controllers\Admin\RoleController;
 use App\Http\Controllers\Admin\PermissionController;
 use App\Http\Controllers\HomeController;
+use Illuminate\Foundation\Auth\EmailVerificationRequest;
 use Illuminate\Support\Facades\Route;
 
 Route::name('admin.')->prefix('admin')->group(function () {
@@ -13,16 +14,24 @@ Route::name('admin.')->prefix('admin')->group(function () {
         Route::get('form-send-email', [UserController::class, 'getMailForm'])->name('form-send-email');
         Route::post('send', [UserController::class, 'sendMail'])->name('send');
     });
-    Route::resource('user', UserController::class);
+    Route::resource('user', UserController::class)->middleware(['auth', 'verified']);
     Route::resource('role', RoleController::class);
     Route::resource('category', CategoryController::class);
     Route::resource('product', ProductController::class);
     Route::resource('permission', PermissionController::class);
 });
 
-Auth::routes();
+Auth::routes(['verify' => true]);
 Route::get('/home', [HomeController::class, 'index'])->name('home');
 
 Route::get('/', function () {
     return redirect('/home');
 });
+Route::get('/email/verify', function () {
+    return view('auth.verify');
+})->middleware('auth')->name('verification.notice');
+Route::get('/email/verify/{id}/{hash}', function (EmailVerificationRequest $request) {
+    $request->fulfill();
+
+    return redirect('/home');
+})->middleware(['auth', 'signed'])->name('verification.verify');
