d9eef6eeaa897a42e2909c697c42a906345afdd5
longfn <longpn.golsoftvn@gmail.com>
2022-07-27 04:56:39 +0000

    Fresh creation of authorization system with Vue scaffold

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\Admin\CategoryController;
 use App\Http\Controllers\Admin\ProductController;
 use App\Http\Controllers\Admin\RoleController;
 use App\Http\Controllers\Admin\PermissionController;
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
 Route::name('admin.')->prefix('admin')->group(function () {
@@ -17,4 +18,11 @@ Route::name('admin.')->prefix('admin')->group(function () {
     Route::resource('category', CategoryController::class);
     Route::resource('product', ProductController::class);
     Route::resource('permission', PermissionController::class);
+});
+
+Auth::routes();
+Route::get('/home', [HomeController::class, 'index'])->name('home');
+
+Route::get('/', function () {
+    return redirect('/home');
 });
