99872a653e5d6d096f7bdeb810434b1b3c70a4be
longfn <longpn.golsoftvn@gmail.com>
2022-07-28 05:30:54 +0000

    Use pint

--- app/Http/Controllers/Auth/VerificationController.php
+++ app/Http/Controllers/Auth/VerificationController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
-use App\Providers\RouteServiceProvider;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Auth\Events\Verified;
