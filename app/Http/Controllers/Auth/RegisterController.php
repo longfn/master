42d3a57406602b58974cb496a2af6b92cd58497e
longfn <longpn.golsoftvn@gmail.com>
2022-08-02 02:43:38 +0000

    Fix: Unused variable

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -44,7 +44,7 @@ class RegisterController extends Controller
 
     public function register(Request $request)
     {
-        $validator = $this->validator($request->all())->validate();
+        $this->validator($request->all())->validate();
 
         event(new Registered($user = $this->create($request->all())));
 
